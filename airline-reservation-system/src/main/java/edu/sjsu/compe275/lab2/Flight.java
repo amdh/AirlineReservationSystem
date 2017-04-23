45d9f8037d72ca24f65b408db393ea898fbed772
amrutadh <amrutadh@gmail.com>
2017-04-22 17:41:13 -0700

    ok

--- airline-reservation-system/src/main/java/edu/sjsu/compe275/lab2/Flight.java
+++ airline-reservation-system/src/main/java/edu/sjsu/compe275/lab2/Flight.java
@@ -2,6 +2,10 @@ package edu.sjsu.compe275.lab2;
 
 import java.util.Date;
 import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 /**
  * Created by Amruta on 4/15/2017.
